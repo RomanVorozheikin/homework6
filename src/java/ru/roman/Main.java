af74b85255fb568d0901ad03cbcd72a1ad6104c1
roma <rommur5@mail.ru>
2023-04-17 16:41:56 +0300

    2задание

--- src/java/ru/roman/Main.java
+++ src/java/ru/roman/Main.java
@@ -3,8 +3,9 @@ package ru.roman;
 public class Main {
     public static void main(String[] args) {
 
-        //Задание 1
-        for (int i=1;i<=10;i=i+1){
+        //Задание 2
+        for (int i = 10; i >= 1; i = i - 1) {
             System.out.println(i);
         }
     }
+}
